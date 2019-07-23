d2d445b89e7e9a95da26a3ddf12971961c09ae53
Gurman Singh <ganz.singh@hotmail.com>
2019-07-23 12:23:47 +1200

    Add files via upload

---
+++ Speed Test/Test/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace Test
+{
+    public enum ImageOrientation
+        {
+            landscape,
+            portrait
+        }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            const int demeritPointPerKm = 5;
+
+
+            Console.WriteLine("Please enter the speed limit: ");
+            var limit = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter your speed: ");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (speed <= limit)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                var demritPoint = (speed - limit) / demeritPointPerKm;
+                if (demritPoint >= 12)
+                {
+                    Console.WriteLine("Suspended");
+                }
+                    else
+                {
+                    Console.WriteLine("Demerit Points: " + demritPoint);
+                }
+
+                
+            }
+        }
+
+               
+
+           
+            
+        }
+            
+
+            
+
+        }
+            
+    
+
